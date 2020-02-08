b77ed4d1b472aa185365f9876b417130370c85c3
olivercastroteran <olivercastroteran@gmail.com>
2020-02-08 01:48:01 -0400

    create and configure project

---
+++ src/theme/styles.js
@@ -0,0 +1,68 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Constants from './constants';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${Constants.drawerWidth}px)`,
+    marginLeft: Constants.drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: Constants.drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: Constants.drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -Constants.drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  addButton: {
+    fontSize: 60,
+    margin: theme.spacing(2),
+    bottom: 0,
+    right: 0,
+    position: 'absolute',
+  },
+}));
+
+export default useStyles;
---
+++ src/views/layout/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import clsx from 'clsx';
+import { useTheme } from '@material-ui/core/styles';
+import { Drawer, CssBaseline, AppBar, Icon, Toolbar, List, Typography, Divider, IconButton,
+  ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Menu, CollectionsBookmark, ChevronLeft, ChevronRight, AddCircle } from '@material-ui/icons';
+import useStyles from '../../theme/styles';
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Course description interface
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeft /> : <ChevronRight />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['course #1', 'course #2', 'course #3'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon><CollectionsBookmark /></ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <div>
+          <Icon><AddCircle color="primary" className={classes.addButton} /></Icon>
+        </div>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        { children }
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
